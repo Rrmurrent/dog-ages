4522c785c0a22e79ea78f899fdcefb602c9e33d2
Smarty Pants <amcurrent@hotmail.com>
2019-12-13 22:56:15 -0600

    added main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+let myAge = 2;
+let earlyYears = 2;
+earlyYears *= 10.5;
+console.log(earlyYears);
+let laterYears = myAge - 2;
+console.log(myAge);
+console.log(laterYears);
+laterYears *= 4;
+console.log (laterYears);
+let myAgeInDogYears = earlyYears + laterYears;
+console.log(myAgeInDogYears);
+myName = 'Frida Kahlo'.toLowerCase();
+console.log(myName);
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
